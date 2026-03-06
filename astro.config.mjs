acff518533abf6bf03ae74a54ca8179e0f2826bf
Ela Sobol <elaksobol@gmail.com>
2026-03-05 23:48:22 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,9 @@
+import { defineConfig } from 'astro/config';
 
+export default defineConfig({
+  // This tells Astro your public URL
+  site: 'https://elaksobol.github.io',
+  // This tells Astro the folder name on GitHub (must match your repo name)
+  base: '/personal-site', 
+  output: 'static',
+});
